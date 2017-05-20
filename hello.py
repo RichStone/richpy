485c6485d02d017271d31f075cdbffdc91d40328
Rich Stone <richard.had@hotmail.de>
2017-05-20 22:08:44 +0200

    add dynamic route appendix

--- hello.py
+++ hello.py
@@ -6,5 +6,9 @@ print(app)
 def index():
     return '<h1>RichPy!</h1>'
 
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hey, %s!</h1>' % name
+
 if __name__ == '__main__':
     app.run(debug=True)
