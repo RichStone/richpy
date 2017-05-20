bd9b4369a4693844325707ad6fe74f115a34da45
Rich Stone <richard.had@hotmail.de>
2017-05-20 22:19:50 +0200

    add browser checker

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 print(app)
 
@@ -9,6 +11,11 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hey, %s!</h1>' % name
+
+@app.route('/agent')
+def agent():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s</p>' % user_agent
 
 if __name__ == '__main__':
     app.run(debug=True)
