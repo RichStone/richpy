33e4118703575f43e971f445e4232748b64e066b
Rich Stone <richard.had@hotmail.de>
2017-05-26 20:37:42 +0200

    introduce base structure for a general template for the website & add 404 + 500 error pages

--- hello.py
+++ hello.py
@@ -1,9 +1,22 @@
-from flask import Flask, redirect, render_template
-from flask import request
+from flask import Flask, redirect, request, render_template
+from flask_script import Shell, Manager
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
-print(app)
+shell = Shell(use_ipython=False)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 @app.route('/')
 def index():
@@ -23,4 +36,4 @@ def redir():
     return redirect('https://www.amazon.com')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
