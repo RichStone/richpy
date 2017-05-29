dbadd8ad78ce634a4f48a2850385a192afa22e98
Rich Stone <richard.had@hotmail.de>
2017-05-29 20:11:21 +0200

    configure database & add Role and User model

--- hello.py
+++ hello.py
@@ -5,12 +5,43 @@ from datetime import datetime
 from flask_wtf import Form
 from wtforms import IntegerField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from pathlib import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+
+# configure data base
+app.config['SQLALCHEMY_DATABASE_URI']=\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
+
 app.config['SECRET_KEY'] = 'YphciR469$$'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+    users = db.relationship('User', backref='role')
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
 class AgeForm(Form):
     age = IntegerField('We need your age for the AI: ', validators=[DataRequired()])
