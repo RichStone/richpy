d780d37617d1c77ccfc300f88bd2b579202d73d9
Rich Stone <richard.had@hotmail.de>
2017-06-01 19:24:29 +0200

    separate configs from main application

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask, redirect, request, render_template, session, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_mail import Mail
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+
+bootstrap = Bootstrap()
+moment = Moment()
+db = SQLAlchemy()
+mail = Mail()
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+
+    # attach routes and custom error pages
+
+    return app
+
+
+
