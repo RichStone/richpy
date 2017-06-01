81bd38c66836c2cec32d21de73eb1abf5152f74f
Rich Stone <richard.had@hotmail.de>
2017-06-01 13:57:18 +0200

    add ansynchronous mail sending and mail templates

--- hello.py
+++ hello.py
@@ -10,6 +10,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 import os
 from flask_mail import Mail, Message
+from threading import Thread
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -22,13 +23,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'YphciR469$$'
 
 # configure email notifications
-app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
-app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
 app.config['MAIL_SERVER'] = 'smtp-mail.outlook.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[richpy]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <richard.had@hotmail.de>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
@@ -58,16 +60,23 @@ class User(db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
 class DataVultureForm(Form):
-    name = StringField('Name required for the database: ', validators=[DataRequired(), Length(min=1, max=20)])
+    name = StringField('Name required for the database: ',
+                       validators=[DataRequired(), Length(min=1, max=20)])
     age = IntegerField('We need your age for the AI: ', validators=[DataRequired()])
     submit = SubmitField('Feed')
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 def send_email(to, subject, template, **kwargs):
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
                   sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    mail.send(msg)
+    thread = Thread(target=send_async_email, args=[app, msg])
+    thread.start()
+    return thread
 
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
@@ -93,7 +102,8 @@ def index():
             db.session.add(user)
             session['known'] = False
             if app.config['FLASKY_ADMIN']:
-                send_email(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
+                send_email(app.config['FLASKY_ADMIN'],
+                           'New User', 'mail/new_user', user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
