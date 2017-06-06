cd6cfe46e54aeb22165da820bbbdcbb714c4ef30
Rich Stone <richard.had@hotmail.de>
2017-06-06 17:57:25 +0200

    add password functionality to User model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Role(db.Model):
@@ -19,3 +20,16 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
