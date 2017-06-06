dc3ff120654537bf47020fca130ffe300c23cb70
Rich Stone <richard.had@hotmail.de>
2017-06-06 18:23:23 +0200

    add login blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ moment = Moment()
 db = SQLAlchemy()
 mail = Mail()
 
+
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
@@ -23,5 +24,8 @@ def create_app(config_name):
     # attach routes and custom error pages
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
