234716d631c7538a66debcf2c431c4956a8e1146
Rich Stone <richard.had@hotmail.de>
2017-06-09 20:39:48 +0200

    add broken login version

--- app/main/views.py
+++ app/main/views.py
@@ -1,29 +1,7 @@
-from datetime import datetime
-from flask import render_template, session, redirect, url_for, request, flash, current_app
-from .. import db
-from ..models import User
-from ..email import send_email
+from flask import render_template
 from . import main
-from .forms import DataVultureForm
 
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/')
 def index():
-    name = 'AFD!'
-    return render_template('user.html', name=name)
-
-
-@main.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
-
-
-@main.route('/agent')
-def agent():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Your browser is %s</p>' % user_agent
-
-
-@main.route('/redir')
-def redir():
-    return redirect('https://www.amazon.com')
+    return render_template('index.html')
