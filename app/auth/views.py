030e2d734f78def7b68c6de3729f1e6ece8d0ab0
Rich Stone <richard.had@hotmail.de>
2017-06-10 18:53:14 +0200

    delete obsolete index form

--- app/auth/views.py
+++ app/auth/views.py
@@ -13,6 +13,7 @@ def login():
             login_user(user, form.remember_me.data)
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or password.')
+    form='asfdasdf'
     return render_template('auth/login.html', form=form)
 
 @auth.route('/logout')
