8c732545c0a8c1216efdb90d710d5ce48e9bf0bc
Rich Stone <richard.had@hotmail.de>
2017-07-03 19:01:53 +0200

    adjust form to modell

--- app/todo/forms.py
+++ app/todo/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import Form
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, DateTimeField, BooleanField
 from wtforms.validators import DataRequired, Length
 
 
 class NewTaskForm(Form):
-    new_task = StringField('New Task: ', validators=[DataRequired(), Length(1, 264)])
+    title = StringField('Title', validators=[DataRequired(), Length(1, 64)])
+    description = StringField('Description', validators=[DataRequired(), Length(1, 256)])
+    due = DateTimeField('Due Date', render_kw={"placeholder":"Format: %Y-%m-%d %H:%M:%S"})
     submit = SubmitField('do it!')
